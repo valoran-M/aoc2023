4d8b1db98e66164d6ed5013fb0ad3dd696e41d61
valomay <mvalo@proton.me>
2023-12-01 11:30:27 +0100

    start first

--- bin/main.ml
+++ bin/main.ml
@@ -3,4 +3,4 @@
 let () =
   let file  = Sys.argv.(1) in
   let lines = Aoc2023.Utils.read_file file in
-  Aoc2023.D1.solve lines
+  Aoc2023.D1.solve_star lines
--- lib/d1.ml
+++ lib/d1.ml
@@ -29,5 +29,45 @@ let solve (input : string list) =
   in
   Printf.printf "%d\n" res
 
-(* let solve_star (input : string list) = *)
+(* --------------------------------------------------------------------- Star *)
+
+let number = [
+    ("one", 1); ("two", 2); ("three", 3); ("four", 4); ("five", 5);
+    ("six", 6); ("seven", 7); ("eight", 8); ("nine", 9)
+  ]
+
+let get_number line =
+  let _, f, l =
+    String.fold_left (fun (i, f, l) c ->
+      let d = 
+        if is_digit c
+        then
+          Some (to_int c)
+        else
+          match List.find_opt (fun (n, _) ->
+            try
+              let s = String.sub line i (String.length n) in
+              String.equal s n
+            with _ -> false
+          ) number with
+          | None -> None
+          | Some (_, d) -> Some d
+        in
+        match d, f with
+        | None, _        -> (i + 1,      f, l)
+        | Some d, None   -> (i + 1, Some d, d)
+        | Some d, Some _ -> (i + 1,      f, d)
+    ) (0, None, 0) line
+  in
+  match f with
+  | Some f -> f * 10 + l
+  | None   -> assert false
+
+let solve_star (input : string list) =
+  let res =
+    List.fold_left (fun acc line ->
+      acc + get_number line
+    ) 0 input
+  in
+  Printf.printf "%d\n" res
 
